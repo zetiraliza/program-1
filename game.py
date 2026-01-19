587dcd1949791411ca2c6df8db6f6746487654ae
zetiraliza <zetiraliza@gmail.com>
2026-01-19 06:07:03 +0000

    game version 1.0

---
+++ game.py
@@ -0,0 +1,13 @@
+# Aplikasi game secret number
+
+secret_number = 15
+
+guess number = int(input("Masukkan Tebak Angka: "))
+
+while guess_number != secret_number
+    print ("Tebakan Salah! Silahkan Coba Lagi!")
+    guess number = int(input("Masukkan Tebak Angka: "))
+
+print ("Selamat...! Tebakan Anda Benar!!")
+print ("Kode ini saya buat di Codespace")
+    
